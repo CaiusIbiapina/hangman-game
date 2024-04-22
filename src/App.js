0a652764cd0316d439db91369a8a4fac87f781c7
Caius <ibiapina.caius@gmail.com>
2024-04-22 21:40:24 +0100

    jogo iniciado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import fotoinicio from './assets/forca0.png'
+import palavras from './palavras'
+import { useState } from 'react';
 
 function App() {
   const alfabeto = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  const [jogoIniciado, setJogoIniciado] = useState(false);
+  const [palavra, setPalavra] = useState('');
+  const [palavraAdivinhada, setPalavraAdivinhada] = useState('');
+  
+  const iniciarJogo = () => {
+    setJogoIniciado(true);
+    const palavraSorteada = palavras[Math.floor(Math.random() * palavras.length)];
+    setPalavra(palavraSorteada);
 
+    // Criar a palavra a ser adivinhada com underlines
+    let palavraComUnderlines = '';
+    for (let i = 0; i < palavraSorteada.length; i++) {
+      palavraComUnderlines += '_  ';
+    }
+    setPalavraAdivinhada(palavraComUnderlines);
+
+  };
 
   return (
     <div className="App">
@@ -13,19 +31,24 @@ function App() {
           <img src={fotoinicio} className='foto-forca' alt='imagem da forca' />
         </div>
 
-        <button className='escolher'>ESCOLHER PALAVRA</button>
+        <div className='topo-lado-direito'>
+          <button className='escolher' disabled={jogoIniciado} onClick={iniciarJogo}>ESCOLHER PALAVRA</button>
+          {jogoIniciado && (
+            <div className='palavra-adivinhada'>{palavraAdivinhada}</div>
+          )}
+        </div>
       </div>
 
       <div className='letras'>
       {alfabeto.map((letra, index) => (
-          <button className='botao-letra' key={index}>{letra}</button>
+          <button className={jogoIniciado ? 'botao-letra-on' : 'botao-letra-off'} key={index} disabled={!jogoIniciado}>{letra}</button>
         ))}
       </div>
 
       <div className='fundo'>
         <h1>Já sei a palavra!</h1>
-        <input placeholder='Digite a palavra' className='digite-palavra'></input>
-        <button className='botao-chutar'>Chutar</button>
+        <input placeholder='Digite a palavra' className='digite-palavra' disabled={!jogoIniciado}></input>
+        <button className='botao-chutar' disabled={!jogoIniciado}>Chutar</button>
       </div>
     </div>
   );
